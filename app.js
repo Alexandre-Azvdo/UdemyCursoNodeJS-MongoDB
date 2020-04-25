5cf63f4833260fa5286b32532d2ea398a00662a2
Alexandre-Azvdo <azvdo.ads@gmail.com>
2020-04-24 23:33:06 -0300

    Adicao da estrategia com Consign e refatoracao das rotas e conexao com DB

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server');
 
-var rota_home = require('./app/routs/home')(app);
+//var rota_home = require('./app/routes/home')(app);
 
-var rota_noticias = require('./app/routs/noticias')(app);
+//var rota_noticias = require('./app/routes/noticias')(app);
 
-var rota_inclusao_noticia = require('./app/routs/formulario_inclusao_noticia')(app);
+//var rota_inclusao_noticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function () {
     console.log('Servidor ON...');
